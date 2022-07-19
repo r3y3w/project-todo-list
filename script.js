d3f2de396910d0887e1d93841eb51cb5e5426887
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-07-19 01:01:35 -0400

    add comments to index.html and script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function addTask() {
+    // Constant list element or listEL will store a new 'li' element created by the createElement()
     const listEl = document.createElement('li')
+   
+    // Constant inputValve will store input value entered by the usere in the index.HTML file input tag element.
     const inputValue = document.getElementById('myInput').value
+   
+    // Constant text will store the value stored in inputValue
     const text = document.createTextNode(inputValue)
+
+    // Line Below: The function appendChild, appends a child element. In example <li>  to the parent element  <ul>
     listEl.appendChild(text)
     document.getElementById('myUL').appendChild(listEl)
     document.getElementById('myInput').value=''
@@ -15,7 +22,8 @@ function addTask() {
     for (let i = 0; i < close.length; i++) {
         close[i].onclick = function (){
             const div = this.parentElement
-            div.style.display = 'none'
+            
+            div.style.display = 'none'  // will do the effect of deleting the completed task.
         }
     }
 }
@@ -41,12 +49,16 @@ for (let i = 0; i < close.length; i++) {
     }
 }
 
-const list = document.querySelector("ul")
+const list = document.querySelector("ul") // Query Selector returns matched element <ul>
 
-list.addEventListener("click", 
+/* 
+addEventListener() will be called  whenever a click over an LI happnes.
+list.addEventListener() has three parameters. param 1 = click, param 2 = anonmymous function, param 3 = false
+*/
+list.addEventListener("click",   
     function(event) {
         if(event.target.tagName === "LI") {
-        event.target.classList.toggle("checked")
+        event.target.classList.toggle("checked")  // Will Create check mark effect
     }
 },
     false
